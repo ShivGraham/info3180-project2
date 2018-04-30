ece3c8515798155a2b3d8b2a06043d49fa1cb20b
Shivaun Graham <shivz@rocketmail.com>
2018-04-30 05:46:05 +0000

    updated Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "Y9%cv'_Sm9u=LAi"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:admin@localhost/photogram"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wavmilsozcqewx:585eda1d7fca794c6ddb5209d8a1ec616c20ea85819b664fbe361ccd12a3cda8@ec2-54-83-1-94.compute-1.amazonaws.com:5432/dcdkth4ikprp9s'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
